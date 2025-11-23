6c8a58ad1abe7dd872f124fff32497379a3afdbe
nikhiltomar2690 <nikhiltomar2690@gmail.com>
2025-11-23 13:15:26 +0530

    node-env enum added

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 dotenv.config();
 
 const envSchema = z.object({
-  NODE_ENV: z.string().default("development"),
+  NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
 
   PORT: z.coerce.number().int().positive(),
 
